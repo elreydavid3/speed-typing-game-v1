71e6e1e806daeea0bf53e18e978b692217889424
David Jones <kingdavidjones@Davids-MacBook-Pro.local>
2020-07-21 17:27:38 -0600

    Updates

--- precache-manifest.734e6a77b6f49bc4c9574a05f1f8e5c3.js
+++ precache-manifest.734e6a77b6f49bc4c9574a05f1f8e5c3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e3ab93369d9ad235fa42261a8dcd2378",
+    "revision": "d97fbf30108cda79a8e27b88b659b0fb",
     "url": "/speed-typing-game-v1/index.html"
   },
   {
-    "revision": "04afc79a9d149a606804",
+    "revision": "3ed486089a0763d43d8c",
     "url": "/speed-typing-game-v1/static/css/main.7a44155f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/speed-typing-game-v1/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "04afc79a9d149a606804",
-    "url": "/speed-typing-game-v1/static/js/main.f4892692.chunk.js"
+    "revision": "3ed486089a0763d43d8c",
+    "url": "/speed-typing-game-v1/static/js/main.ca299ac3.chunk.js"
   },
   {
     "revision": "77529314dd60ce37a8ab",
